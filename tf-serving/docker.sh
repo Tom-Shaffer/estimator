c1ee6be4e3802336de294a8b5af4bea66e7728c7
Tom-Shaffer <ShafferTom0@gmail.com>
2023-03-14 22:12:22 -0500

    Getting tf-serving working (prediction still bad)

--- tf-serving/docker.sh
+++ tf-serving/docker.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
 # Start TensorFlow Serving container and open the REST API port
-docker run -t --rm -p 8500:8501 \
-    -v "/Users/tomshaffer/estimator/tf-serving/models/estimator_model" \
-    -e MODEL_NAME=estimator_model \
-    emacski/tensorflow-serving &
+docker run -t --rm -p 8501:8501 \
+  --mount type=bind,source="$(pwd)"/models,target=/models \
+  -e MODEL_NAME=estimator -it emacski/tensorflow-serving:latest &
+
+# Query the model using the predict API
+# Variables are as follows for the API call
+#   building_area: a numerical variable representing the area of the building.
+#   building_volume: a numerical variable representing the volume of the building.
+#   deadline_months: a numerical variable representing the number of months until the project deadline.
+#   building_type_industrial: a binary variable representing whether the building type is industrial or not.
+#   building_type_residential: a binary variable representing whether the building type is residential or not.
+#   building_type_commercial: a binary variable representing whether the building type is commercial or not.
+#   efficiency_level_high: a binary variable representing whether the efficiency level is high or not.
+#   efficiency_level_medium: a binary variable representing whether the efficiency level is medium or not.
+#   efficiency_level_low: a binary variable representing whether the efficiency level is low or not.
+#   hvac_type_geothermal: a binary variable representing whether the HVAC type is geothermal or not.
+#   hvac_type_gas: a binary variable representing whether the HVAC type is gas or not.
+#   hvac_type_oil: a binary variable representing whether the HVAC type is oil or not.
+#   hvac_type_electric: a binary variable representing whether the HVAC type is electric or not.
+curl -d '{ "instances": [[8222,745532,16,1,0,0,1,0,0,1,0,0,0]]}' \
+-X POST http://localhost:8501/v1/models/estimator:predict
