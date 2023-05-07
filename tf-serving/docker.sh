953f9ad6a5b4e9b1d37e8421f3d34376e0c4e64f
Tom-Shaffer <ShafferTom0@gmail.com>
2023-05-07 18:58:12 -0500

    Updating TF-Serving with updated model

--- tf-serving/docker.sh
+++ tf-serving/docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start TensorFlow Serving container and open the REST API port
-docker run -t --rm -p 8501:8501 \
+sudo docker run -t --rm -p 8501:8501 \
   --mount type=bind,source="$(pwd)"/models,target=/models \
   -e MODEL_NAME=estimator -it emacski/tensorflow-serving:latest &
 
@@ -16,20 +16,20 @@ docker run -t --rm -p 8501:8501 \
 #   efficiency_level_high: a binary variable representing whether the efficiency level is high or not.
 #   efficiency_level_medium: a binary variable representing whether the efficiency level is low or not.
 #   efficiency_level_low: a binary variable representing whether the efficiency level is medium or not.
-#   hvac_type_forced air: a binary variable representing whether the HVAC type is gas or not.
-#   hvac_type_boiler: a binary variable representing whether the HVAC type is geothermal or not.
-#   hvac_type_heat pump: a binary variable representing whether the HVAC type is electric or not.
 #   hvac_type_geothermal: a binary variable representing whether the HVAC type is oil or not.
+#   hvac_type_heat pump: a binary variable representing whether the HVAC type is electric or not.
+#   hvac_type_boiler: a binary variable representing whether the HVAC type is geothermal or not.
+#   hvac_type_forced air: a binary variable representing whether the HVAC type is gas or not.
 
-        "budget": 6707.3,
-        "building_type": "residential",
-        "building_area": 3353,
-        "building_volume": 194156,
-        "deadline_months": 6,
-        "efficiency_level": "medium",
-        "hvac_type": "heat pump"
+        "budget": "818458",
+        "building_type": "building.commercial",
+        "building_area": "38497",
+        "building_volume": "7553362",
+        "deadline_months": "9",
+        "efficiency_level": "efficiency.medium",
+        "hvac_type": "hvac.heatpump"
 
 # This query should result in something close to the true value
-# "predictions": [[16443.64]]
-curl -d '{ "instances": [[3353,194156,6,0,0,1,0,1,0,0,0,1,0]]}' \
+# "predictions": [[753167.875]]
+curl -d '{ "instances": [[38497,7553362,9,0,1,0,0,1,0,0,1,0,0]]}' \
 -X POST http://localhost:8501/v1/models/estimator:predict
