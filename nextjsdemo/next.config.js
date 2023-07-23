8bfc18b680674f7ab05486698ab240d753cb3de0
Tom-Shaffer <ShafferTom0@gmail.com>
2023-07-23 16:04:17 -0500

    Adding post build step to migrate model / public

--- nextjsdemo/next.config.js
+++ nextjsdemo/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    staticFolder: '/public', // Change this to the correct path for your project
+  },
 }
 
 module.exports = nextConfig
